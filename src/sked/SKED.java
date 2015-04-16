d463bd76b921e6b5816e87653f348f3a4ce08a26
12bcookj <12bcookj@NTCSFileServer.ntcs.ca>
2015-04-16 13:57:38 -0400

    Trying to practice committing

---
+++ src/sked/SKED.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sked;
+
+/**
+ *
+ * @author 12bcookj
+ */
+public class SKED {
+
+    /**
+     * 
+     * Julia has edited this code
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
