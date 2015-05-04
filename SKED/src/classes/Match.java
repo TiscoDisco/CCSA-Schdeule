c572d12e221f9389217dfbf668a36b881c6062db
TonyChen <TonyChen@192.168.0.26>
2015-05-04 09:30:14 -0400

    Changes to classes

--- SKED/src/classes/Match.java
+++ SKED/src/classes/Match.java
@@ -10,5 +10,30 @@ package classes;
  * @author TonyChen
  */
 public class Match {
+    /*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+    Team teamA;
+    Team teamB;
     
+    public Match(Team a, Team b){ //constructor
+        teamA=a;
+        teamB=b;
+    }
+    public Team getTeamA(){ // return team A 
+        return teamA;
+    }
+    public Team getTeamB(){ // return team B
+        return teamB;
+    }
+    public boolean equals(Match m){ //tells if the match is the same
+        if(m.getTeamA().equals(teamA)&&m.getTeamB().equals(teamB)||m.getTeamB().equals(teamA)&&m.getTeamA().equals(teamB)){
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
--- SKED/src/classes/Team.java
+++ SKED/src/classes/Team.java
@@ -10,5 +10,28 @@ package classes;
  * @author 12akimr
  */
 public class Team {
-    
+
+    String church;
+    String name;
+
+    public Team(String n, String c) { // constructor
+        name = n;
+        church = c;
+    }
+
+    public String getChurch() { // return church name
+        return church;
+    }
+
+    public String getName() { // return team name
+        return name;
+    }
+
+    public boolean equals(Team t) { // tells if the teams are the same
+        if (t.getChurch().equals(church) && t.getName().equals(name)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
