b0a659820a95f135f67c6a8fc30040a1f398bb3a
User <User@User-PC>
2015-05-07 23:30:53 -0400

    modifications to classes package

--- SKED/src/classes/ScheduleProtoTony.java
+++ SKED/src/classes/ScheduleProtoTony.java
@@ -11,5 +11,40 @@ package classes;
  */
 import java.util.*;
 public class ScheduleProtoTony {
-    //get the list of matches
+    public static void main(String[] args){
+        //get the list of matches
+        //scheduling
+        /*check availability after each of the following steps
+        pick park
+        saturday
+        pick match
+        pick match with one of the two teams
+        pick match with another one of the two teams
+        sunday
+        pick match
+        pick match with one of the two teams
+        pick match with another one of the two teams
+        */
+        /*Possible Matchings
+        Most Optimal
+            AB AC CD
+            EF EG GH
+        least optimal
+            AB CD EF
+            GH IJ LM
+        */
+        //umpiring schedule
+        /*
+            Most Optimal
+            3 2
+            and
+            3 1 1
+            
+            Make sure every team gets a triple header
+            Some teams will get another double header
+            Other teams will get two singles
+        */
+        
+    }
+    
 }
