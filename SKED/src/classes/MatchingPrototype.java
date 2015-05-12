3bb5e656fcc3cec154329507202bc846ed8ad181
12bmyert <12bmyert@NTCSFileServer.ntcs.ca>
2015-05-12 14:05:02 -0400

    Updated comments made by Trent

--- SKED/src/classes/MatchingPrototype.java
+++ SKED/src/classes/MatchingPrototype.java
@@ -10,7 +10,7 @@ import java.util.*;
 /**
  *
  * @author TonyChen
- * comments by Trent and Naomi
+ * comments made by Trent and Naomi
  */
 public class MatchingPrototype {
 
@@ -31,17 +31,17 @@ public class MatchingPrototype {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.println("How many teams?");
-        numTeams = Integer.parseInt(in.nextLine());
+        numTeams = Integer.parseInt(in.nextLine()); //get user input, for testing purposes
         System.out.println("How many games are played by each team?");
-        play = Integer.parseInt(in.nextLine());
+        play = Integer.parseInt(in.nextLine()); //get user input, for testing purposes
         noplay = numTeams - play - 1;
-        for (int i = 0; i < numTeams; i++) { //store list of churches
+        for (int i = 0; i < numTeams; i++) { //store list of churches in churchList
             System.out.println("Church name " + i);
             String church = in.nextLine();
             if (!churchList.contains(church)) {
                 churchList.add(church);
             }
-            System.out.println("Team name " + i); //store list of teams 
+            System.out.println("Team name " + i); //store list of teams in teamList
             String team = in.nextLine();
             teamList.add(new Team(team, church));
         }
@@ -49,60 +49,63 @@ public class MatchingPrototype {
 //            System.out.println(thisTeam.name);
 //        }
 
-        Random rand = new Random(); //randomize list of churches
+        Random rand = new Random(); //randomize list of churches that are in the churchList array. The randomized churches are put into a new array called newChurchList
         while (!churchList.isEmpty()) {
             int i = rand.nextInt(churchList.size());
             newChurchList.add(churchList.get(i));
             churchList.remove(i);
         }
 
-        while (teamList.size() > 0) { //randomize list of teams
+        while (teamList.size() > 0) { //randomize list of teams that are in the teamList array. The randomized teams are put into a new array called newTeamList
             int i = rand.nextInt(teamList.size());
             newTeamList.add(teamList.get(i));
             teamList.remove(i);
         }
 
-        for (String thisChurch : newChurchList) { //sort teams from the same church together
+        for (String thisChurch : newChurchList) { //sort teams from the same church to ensure that they will not play eachother
             for (Team thisTeam : newTeamList) {
                 if (thisTeam.getChurch().equals(thisChurch)) {
-                    sortedTeamList.add(thisTeam);
+                    sortedTeamList.add(thisTeam); //Puts the sorted teams into a new array called sortedTeamList
                 }
             }
         }
 
-        for (int i = 0; i < sortedTeamList.size(); i++) { // create an array including all possible match ups
+        for (int i = 0; i < sortedTeamList.size(); i++) { // create an array including all possible match ups. The match ups are put into the playList array
             for (int j = i + 1; j < sortedTeamList.size(); j++) {
                 playList.add(new Match(sortedTeamList.get(i), sortedTeamList.get(j)));
             }
         }
 
-        for (Match show : playList) {
-            System.out.println(show.getTeamA().getName() + " v " + show.getTeamB().getName());
-        }
+//        for (Match show : playList) {
+//            System.out.println(show.getTeamA().getName() + " v " + show.getTeamB().getName());// used for testing purposes. Displays all possible matches.
+//        }
 
         for (int i = 0; i < sortedTeamList.size(); i++) { //create an array of match ups that will not be played
-            for (int j = 1; j <= noplay / 2; j++) {//count the match ups not played forwards in the array
+            for (int j = 1; j <= noplay / 2; j++) {//count the match ups not played in the forwards direction of the array
                 int pick = i + j;
                 if (pick >= sortedTeamList.size()) {
                     pick -= sortedTeamList.size();
                 }
+                
+                //Match ups for a league with an even number of teams
+                
                 Match currentMatch = new Match(sortedTeamList.get(i), sortedTeamList.get(pick));
                 boolean contains = false;
                 for (Match checkMatch : noPlayList) {
-                    if (checkMatch.equals(currentMatch)) {
+                    if (checkMatch.equals(currentMatch)) {//testing if the current match is in the noPlayList. If it is, the variable, contains, is changed to true
                         contains = true;
                     }
                 }
                 if (!contains) {
-                    noPlayList.add(currentMatch);
+                    noPlayList.add(currentMatch); //if the match is not already in the array then it is added to the noPlayList array
                 }
             }
-            for (int k = 1; k <= noplay / 2; k++) {//count the match ups not played backwards in the array
+            for (int k = 1; k <= noplay / 2; k++) {//count the match ups not played in the backwards direction of the array
                 int pick = i - k;
                 if (pick < 0) {
                     pick += sortedTeamList.size();
                 }
-                Match currentMatch = new Match(sortedTeamList.get(i), sortedTeamList.get(pick)); //test possible match up
+                Match currentMatch = new Match(sortedTeamList.get(i), sortedTeamList.get(pick)); //testing if the current match is in the noPlayList. If it is, the variable, contains, is changed to true
                 boolean contains = false;
                 for (Match checkMatch : noPlayList) {
                     if (checkMatch.equals(currentMatch)) {
@@ -110,10 +113,13 @@ public class MatchingPrototype {
                     }
                 }
                 if (!contains) {
-                    noPlayList.add(currentMatch); //if false, add match up to noPlayList array
+                    noPlayList.add(currentMatch); //if the match is not already in the array then it is added to the noPlayList array
                 }
             }
-            if (noplay % 2 > 0) { //matches the odd match
+            
+            //Match ups for a league with an odd number of teams
+            
+            if (noplay % 2 > 0) { // creates matches for a league with an odd number of teams
                 int pick;
                 if (i % 2 > 0) {
                     pick = i + (noplay / 2) + 1;
@@ -126,7 +132,7 @@ public class MatchingPrototype {
                 if (pick >= sortedTeamList.size()) {
                     pick -= sortedTeamList.size();
                 }
-                Match currentMatch = new Match(sortedTeamList.get(i), sortedTeamList.get(pick));
+                Match currentMatch = new Match(sortedTeamList.get(i), sortedTeamList.get(pick));//testing if the current match is in the noPlayList. If it is, the variable, contains, is changed to true
                 boolean contains = false;
                 for (Match checkMatch : noPlayList) {
                     if (checkMatch.equals(currentMatch)) {
@@ -134,12 +140,12 @@ public class MatchingPrototype {
                     }
                 }
                 if (!contains) {
-                    noPlayList.add(currentMatch);
+                    noPlayList.add(currentMatch);//if the match is not already in the array then it is added to the noPlayList array
                 }
             }
         }
 
-        for (int i = 0; i < playList.size(); i++) { //remove matches not to be played
+        for (int i = 0; i < playList.size(); i++) { //checking if an index of the noPlayList array is equal to an index of the playList array
             boolean contains = false;
             for (int j = 0; j < noPlayList.size(); j++) {
                 if (playList.get(i).equals(noPlayList.get(j))) {
@@ -147,7 +153,7 @@ public class MatchingPrototype {
                 }
             }
             if (contains) {
-                playList.remove(i);
+                playList.remove(i);//if they are the same at a certain index then remove that match up from the playList array
                 i--;
             }
         }
