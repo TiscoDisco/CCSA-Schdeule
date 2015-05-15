b31ab9444d09f8a10a1f176302afd89bc526293e
TonyChen <TonyChen@192.168.0.26>
2015-05-15 14:09:52 -0400

    updates to classes package

--- SKED/src/classes/Division.java
+++ SKED/src/classes/Division.java
@@ -14,39 +14,45 @@ package classes;
 import java.util.*;
 
 public class Division {
-
+    
     ArrayList<Team> teamList = new ArrayList<>();
     ArrayList<Match> matchList = new ArrayList<>();
     ArrayList<Park> parkList = new ArrayList<>();
     String league;
-
+    
     public Division(String d) {
         league = d;
     }
-
+    
     public ArrayList<Team> getTeamList() {
         return teamList;
     }
-
+    
     public ArrayList<Match> getMatchList() {
         return matchList;
     }
-
+    
     public ArrayList<Park> getParkList() {
         return parkList;
     }
-
+    
     public String getLeague() {
         return league;
     }
     
-    public boolean hasNotScheduled(){
-        for(Match m:matchList){
-            if(!m.isScheduled()){
+    public boolean hasNotScheduled() {
+        for (Match m : matchList) {
+            if (!m.isScheduled()) {
                 return false;
             }
         }
         return true;
     }
-
+    
+    private long dateToLong(int y, int m, int d) {
+        Calendar thisCal = Calendar.getInstance();
+        thisCal.set(y, m, d);
+        return thisCal.getTimeInMillis();
+    }
+    
 }
--- SKED/src/classes/SoftDate.java
+++ SKED/src/classes/SoftDate.java
@@ -12,18 +12,21 @@ package classes;
 import java.util.*;
 
 public class SoftDate extends Date {
-
+    
     private int weekNum;
     private Match match1;
     private Match match2;
     private Match match3;
     
-    public SoftDate(int w, int y, int m, int d) {
-        super(y, m, d);
+    
+    public SoftDate(int w, long t) {
+        super(t);
         weekNum = w;
     }
     public boolean equals(SoftDate d){
-        if()
+        if(weekNum == d.weekNum &&){
+            
+        }
         return false;
     }
     
