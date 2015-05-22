5ba415f3f32050c7456dff904fe8fe8319c9aca6
12akims <12akims@NTCSFileServer.ntcs.ca>
2015-05-22 14:05:18 -0400

    not working right now

--- SKED/src/classes/Division.java
+++ SKED/src/classes/Division.java
@@ -14,7 +14,7 @@ public class Division {
     private SoftDate endDate;
     private ArrayList<Team> teamList = new ArrayList<>();
     private ArrayList<Match> matchList = new ArrayList<>();
-    private ArrayList<Park> parkList = new ArrayList<>();
+    //private ArrayList<Park> parkList = new ArrayList<>();
     private ArrayList<SoftDate> dateList = new ArrayList<>();
 
     public Division(SoftDate s, SoftDate e) {
@@ -28,10 +28,6 @@ public class Division {
 
     public ArrayList<Match> getMatchList() {
         return matchList;
-    }
-
-    public ArrayList<Park> getParkList() {
-        return parkList;
     }
 
     public boolean hasNotScheduled() {
@@ -51,46 +47,87 @@ public class Division {
 
     public void schedule(ArrayList<Park> pList) {
         while (hasNotScheduled()) {
-            
+
         }
-        /*check availability after each of the following steps
-         pick park
-         saturday
-         pick match
-         pick match with one of the two teams
-         pick match with another one of the two teams
-         sunday
-         pick match
-         pick match with one of the two teams
-         pick match with another one of the two teams
-         */
+        Scanner in = new Scanner(System.in);
+        int numTeams = teamList.size();
+        int lenArray = 0;
+        int days = 0;
+        int h = 0;
+        for (int i = 1; i <= numTeams; i++) {
+            lenArray += i;
+        }
+        if (lenArray % 3 > 0) {
+            days = lenArray / 3 + 1;
+        } else {
+            days = lenArray / 3;
+        }
+        //String[] teams = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "a"};
+        String[] games = new String[lenArray];
+        ArrayList<String> games1 = new ArrayList(lenArray);
+        ArrayList<String> scheduleArray = new ArrayList(lenArray);
 
-        /*
-         Possible Matchings
-         Most Optimal
-         AB AC CD
-         EF EG GH
-         least optimal
-         AB CD EF
-         GH IJ LM
+        for (int i = 0; i < numTeams; i++) {
+            for (int j = i + 1; j < numTeams; j++) {
+
+            }
+        }
+        scheduleArray.add(games[0]);
+        while (!games1.isEmpty()) {
+            for (int i = 0; i < days; i++) {
+                for (int j = 0; j < 6; j++) {
+                    if (j == 0) {
+                        scheduleArray.add(games[j]);
+                    }
+                    if (j == 1) {
+                        for (int k = 0; k < games.length; i++) {
+                            h = 0;
+                            while ((games[0].substring(0, 0) == games[h].substring(0, 0) || games[0].substring(0, 0) == games[h].substring(1, 1)) && (games[0].substring(1, 1) != games[h].substring(0, 0) || games[0].substring(1, 1) != games[h].substring(1, 1))) {
+                                h += 1;
+                            }
+                        }
+                    }
+                }
+            }
+            /*check availability after each of the following steps
+             pick park
+             saturday
+             pick match
+             pick match with one of the two teams
+             pick match with another one of the two teams
+             sunday
+             pick match
+             pick match with one of the two teams
+             pick match with another one of the two teams
+             */
+
+            /*
+             Possible Matchings
+             Most Optimal
+             AB AC CD
+             EF EG GH
+             least optimal
+             AB CD EF
+             GH IJ LM
         
-         day 1
-         AB 
-         AC or BC or CD
-         CD or CD or DE or EF
+             day 1
+             AB 
+             AC or BC or CD
+             CD or CD or DE or EF
         
-         */
-        //umpiring schedule
+             */
+            //umpiring schedule
         /*
-         Most Optimal
-         3 2
-         and
-         3 1 1
+             Most Optimal
+             3 2
+             and
+             3 1 1
             
-         Make sure every team gets a triple header
-         Some teams will get another double header
-         Other teams will get two singles
-         */
-    }
+             Make sure every team gets a triple header
+             Some teams will get another double header
+             Other teams will get two singles
+             */
+        }
 
+    }
 }
