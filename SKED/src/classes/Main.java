b791d4c8bb2cd9277c2ff7368569b09137fb96a4
User <User@User-PC>
2015-05-24 19:33:16 -0400

    modifications to classes package

--- SKED/src/classes/Main.java
+++ SKED/src/classes/Main.java
@@ -18,30 +18,36 @@ public class Main {
     ArrayList<Park> seniorParkList = new ArrayList<>();
     ArrayList<Park> varsityParkList = new ArrayList<>();
 
+    @SuppressWarnings("empty-statement")
     public static void main(String[] args) {
         //list of playable days
         Scanner in = new Scanner(System.in);
-        Calendar thisCal = Calendar.getInstance();
+        Calendar startCal = Calendar.getInstance();
         System.out.println("start year");
         int sy = Integer.parseInt(in.nextLine());
         System.out.println("start month");
         int sm = Integer.parseInt(in.nextLine());
         System.out.println("start day");
         int sd = Integer.parseInt(in.nextLine());
-        thisCal.set(sy, sm, sd);
-        startDate = new SoftDate(thisCal.getTimeInMillis());
+        startCal.set(sy, sm, sd);
+        Calendar endCal = Calendar.getInstance();
+        startDate = new SoftDate(startCal.getTimeInMillis());
         System.out.println("end year");
         int ey = Integer.parseInt(in.nextLine());
         System.out.println("end month");
         int em = Integer.parseInt(in.nextLine());
         System.out.println("end day");
         int ed = Integer.parseInt(in.nextLine());
-        thisCal.set(ey, em, ed);
-        startDate = new SoftDate(thisCal.getTimeInMillis());
+        endCal.set(ey, em, ed);
+        startDate = new SoftDate(endCal.getTimeInMillis());
         
         //create list of days
-        for(){
-            
+        Calendar doCal = Calendar.getInstance();
+        doCal.set(sy, sm, sd);
+        while(doCal.compareTo(endCal)<=0){
+            if(doCal.get(Calendar.DAY_OF_WEEK)==Calendar.SATURDAY || doCal.get(Calendar.DAY_OF_WEEK)==Calendar.SUNDAY){
+                
+            }
         }
     }
 }
--- SKED/src/classes/Park.java
+++ SKED/src/classes/Park.java
@@ -8,13 +8,11 @@ package classes;
 import java.util.*;
 
 public class Park {
-    private Division division;
     private String name;
     private ArrayList<SoftDate> noPlays = new ArrayList<>();
     
     public Park(String n, Division m) {
         name = n;
-        division = m;
     }
     
     public void addNoPlay(SoftDate d){
@@ -23,9 +21,6 @@ public class Park {
     
     public String getName(){
         return name;
-    }
-    public Division getDivision(){
-        return division;
     }
     
     
