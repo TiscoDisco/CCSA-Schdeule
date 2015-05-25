c9c21e9a6489c6919ad27ad8b874440d21f1bb2e
12akims <12akims@NTCSFileServer.ntcs.ca>
2015-05-25 14:05:26 -0400

    not working right now

--- SKED/src/classes/Division.java
+++ SKED/src/classes/Division.java
@@ -177,47 +177,79 @@ public class Division {
     }
 
     public void schedule(ArrayList<Park> pList) {
-        while (hasNotScheduled()) {
-
-            /*check availability after each of the following steps
-             pick park
-             saturday
-             pick match
-             pick match with one of the two teams
-             pick match with another one of the two teams
-             sunday
-             pick match
-             pick match with one of the two teams
-             pick match with another one of the two teams
-             */
-
-            /*
-             Possible Matchings
-             Most Optimal
-             AB AC CD
-             EF EG GH
-             least optimal
-             AB CD EF
-             GH IJ LM
+        while (hasNotScheduled()) 
+        {
+            for (int i = 0; i <=5; i++)
+            {
+                if (i == 0)
+                {
+                                    
+                }
+                if (i == 1)
+                {
+                                    
+                }
+                if (i == 2)
+                {
+                                    
+                }
+                if (i == 3)
+                {
+                                    
+                }
+                if (i == 4)
+                {
+                                    
+                }
+                if (i == 5)
+                {
+                                    
+                }
+                
+            }
+        }
         
-             day 1
-             AB 
-             AC or BC or CD
-             CD or CD or DE or EF
+    }
+
+    /*check availability after each of the following steps
+     pick park
+     saturday
+     pick match
+     pick match with one of the two teams
+     pick match with another one of the two teams
+     sunday
+     pick match
+     pick match with one of the two teams
+     pick match with another one of the two teams
+     */
+
+    /*
+     Possible Matchings
+     Most Optimal
+     AB AC CD
+     EF EG GH
+     least optimal
+     AB CD EF
+     GH IJ LM
         
-             */
+     day 1
+     AB 
+     AC or BC or CD
+     CD or CD or DE or EF
+        
+     */
             //umpiring schedule
         /*
-             Most Optimal
-             3 2
-             and
-             3 1 1
+     Most Optimal
+     3 2
+     and
+     3 1 1
             
-             Make sure every team gets a triple header
-             Some teams will get another double header
-             Other teams will get two singles
-             */
-        }
+     Make sure every team gets a triple header
+     Some teams will get another double header
+     Other teams will get two singles
+     */
+}
 //        Scanner in = new Scanner(System.in);
 //        int numTeams = teamList.size();
 //        int lenArray = 0;
@@ -258,7 +290,8 @@ public class Division {
 //                    }
 //                }
 //            }
-
-    }
-
-}
+//
+//        }
+//
+//    }
+//}
