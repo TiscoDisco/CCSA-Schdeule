a6acfc918593f8903bf243fd6c61f6e72135ee6f
TheManTheMythTheLegend <trentwmyers@gmail.com>
2015-06-13 23:09:46 -0400

    Comments made

--- SKED/src/classes/Park.java
+++ SKED/src/classes/Park.java
@@ -6,7 +6,7 @@ package classes;
  */
 import java.util.*;
 
-//COMMENT THIS
+
 
 public class Park {
 
@@ -25,6 +25,7 @@ public class Park {
         Calendar doCal = Calendar.getInstance();
         doCal.clear();
         doCal.setTime(startDate);
+        //while the season is still in session, this code adds dates to the available days for games
         while (doCal.getTime().compareTo(endDate) <= 0) {
             if (doCal.get(Calendar.DAY_OF_WEEK) == Calendar.SATURDAY || doCal.get(Calendar.DAY_OF_WEEK) == Calendar.SUNDAY) {
                 dateList.add(new SoftDate(doCal.getTimeInMillis()));
@@ -33,11 +34,11 @@ public class Park {
         }
     }
 
-    public void addNoPlay(SoftDate d, int slot) {
+    public void addNoPlay(SoftDate d, int slot) { // this adds a no play day 
         noPlays.add(d);
     }
 
-    public String getName() {
+    public String getName() { // this gets the name of a string 
         return name;
     }
 
@@ -45,14 +46,14 @@ public class Park {
         return dateList;
     }
 
-    public SoftDate getStartDate() {
+    public SoftDate getStartDate() { // tis gets the start date of the schedule
         return startDate;
     }
 
-    public SoftDate getEndDate() {
+    public SoftDate getEndDate() { // this gets the end date of the schedule
         return endDate;
     }
-
+    // this code ensures that a team will not have to play on a no play date
     public boolean hasPlayDate(SoftDate d) {
         for (SoftDate np : noPlays) {
             if (np.equals(d)) {
@@ -61,7 +62,7 @@ public class Park {
         }
         return true;
     }
-
+    // this code checks specific dates for teams based on days that they can play
     public boolean canPlay(SoftDate d, int s) {
         if (hasPlayDate(d)) {
             for (SoftDate checkDate : dateList) {
